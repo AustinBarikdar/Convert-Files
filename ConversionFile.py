3c4b5ab41144bdf5e4c1110961612349d2deba29
AustinBarikdar <austinbarikdar@gmail.com>
2024-11-27 02:03:25 -0500

    Added a wait time label

--- ConversionFile.py
+++ ConversionFile.py
@@ -10,6 +10,8 @@ result = None
 def file_convert_image(self,file_path,file_type):
     try:
         image = Image.open(file_path)
+
+        self.results.config(text = "Proccessing")
         image.save(f"{file_path[0:file_path.rfind("/") + 1]}Output.{file_type.lower()}", file_type)
 
         self.results.config(text = "Successful!")
@@ -20,6 +22,7 @@ def file_convert_image(self,file_path,file_type):
 def file_convert_audio(self,file_path,file_type,file_name):
     try:
         audio = AudioSegment.from_file(file_path)
+        self.results.config(text = "Proccessing")
         audio.export(str(file_name) + "." + file_type.lower(), format= file_type.lower())
 
         self.results.config(text = "Successful!")
@@ -30,6 +33,7 @@ def file_convert_documents(self,file_path,file_type,file_name):
     extra_args = ['--pdf-engine=xelatex'] 
     try:
         print(str(file_name) + "." + file_type.lower())
+        self.results.config(text = "Proccessing")
         pypandoc.convert_file(file_path, file_type.lower(), outputfile= (str(file_name) + "." + file_type.lower()),extra_args= extra_args)
         self.results.config(text = "Successful!")
     except:
@@ -39,12 +43,15 @@ def file_convert_video(self,file_path,file_type,file_name):
 
     try:
         print(str(file_name) + "." + file_type.lower())
+        self.results.config(text = "Proccessing")
         if file_type.lower() != "mp3":
             clip = VideoFileClip(file_path)
+            
             clip.write_videofile(str(file_name) + "." + file_type.lower(), codec="libx264")
 
         elif file_type.lower() == "mp3":
             clip = VideoFileClip(file_path)
+            
             clip.audio.write_audiofile(str(file_name) + "." + file_type.lower())
 
 
